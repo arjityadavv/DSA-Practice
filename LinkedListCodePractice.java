91d73014cb8222d581af244c59b8170b26adea6e
Arjit Yadav <yadavarjit@gmail.com>
2025-01-16 00:25:08 +0530

    Day5-6 of learning

--- LinkedListCodePractice.java
+++ LinkedListCodePractice.java
@@ -61,6 +61,97 @@ public class LinkedListCodePractice {
         }
     }
 
+    public void deleteNodeAtEnd(){
+        Node current = head;
+        Node prev = null;
+        while(current!=null){
+            if(current.next==null){
+                prev.next = null;
+                break;
+            }else {
+                prev = current;
+                current = current.next;
+                continue;
+            }
+        }
+    }
+
+    public void deleteNoteAtStart(){
+        Node current = head;
+        if(current!=null){
+            if(current.next!=null){
+                Node next_node = current.next;
+                head = next_node;
+                current.next = null;
+            }else{
+                head = null;
+                tail = null;
+            }
+        }else {
+            System.out.println("There is no node present, nothing will be deleted.");
+        }
+
+    }
+
+    public void deleteNodeAtGivenIndex(int index){
+        Node current = head;
+        int count = 1;
+        if(index==1){
+            deleteNoteAtStart();
+        }else{
+            Node before = null;
+            Node after = null;
+            Node deleted = null;
+            while(current!=null){
+                if(count==index-1){
+                    before = current;
+                } else if(count==index){
+                    deleted = current;
+                    if(current.next!=null){
+                        after = current.next;
+                    }else{
+                        after = null;
+                    }
+
+                }
+                current = current.next;
+                count++;
+            }
+            before.next = after;
+            if(after!=null){
+                after.next = null;
+                deleted.next = null;
+            }
+        }
+    }
+
+    public void deleteTheEvenValueNode(){
+        Node current = head;
+        int count = 1;
+        while(current!=null){
+            if(current.val%2==0){
+                deleteNodeAtGivenIndex(count);
+                count = 1;
+            }else{
+                count++;
+            }
+            current = current.next;
+        }
+    }
+
+    public Node reverseLinkedList(){
+        Node currentNode = head;
+        Node prevNode = null;
+        Node nextNode = null;
+        while(currentNode!=null){
+            nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        return prevNode;
+    }
+
     public static void main(String[] args) {
         LinkedListCodePractice linkedList = new LinkedListCodePractice();
         linkedList.addNodeAtEnd(11);
@@ -68,8 +159,26 @@ public class LinkedListCodePractice {
         linkedList.addNodeAtEnd(33);
         linkedList.addNodeAtEnd(44);
         linkedList.addNodeAtStart(55);
+        System.out.println(">>>>>>>>>>>>>>>>>");
         linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
         linkedList.addNodeAtGivenIndex(99, 4);
         linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        linkedList.deleteNodeAtEnd();
+        linkedList.deleteNodeAtEnd();
+        linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        linkedList.deleteNoteAtStart();
+        linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        linkedList.deleteNodeAtGivenIndex(4);
+        linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        linkedList.deleteTheEvenValueNode();
+        linkedList.printLinkedList(head);
+        System.out.println(">>>>>>>>>>>>>>>>>");
+        Node startNode = linkedList.reverseLinkedList();
+        linkedList.printLinkedList(startNode);
     }
 }
