8e9eecf8866e7ec236a2d686ce8a42ff4954f78e
Arjit Yadav <yadavarjit@gmail.com>
2025-01-24 02:48:41 +0530

    Day8-9 of learning

---
+++ CodingQuestionPractice.java
@@ -0,0 +1,17 @@
+//public class CodingQuestionPractice {
+//    public static void main(String[] args) {
+//        int[] array = {-1,2,-3,-5,4,-6};
+//        boolean flag = true;
+//        while(flag){
+//            if(array[i]>array[i+1]){
+//                int temp = array[i+1];
+//                array[i+1] = array[i];
+//                array[i] = temp;
+//                flag = true;
+//            }
+//        }
+//        for(int i: array){
+//            System.out.println(i);
+//        }
+//    }
+//}
--- StackPractice.java
+++ StackPractice.java
@@ -1,85 +1,140 @@
+import javax.swing.*;
 import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class StackPractice {
-    public static Stack<Integer> stackIntegers(){
+    public static Stack<Integer> stackIntegers() {
         Stack<Integer> stack1 = new Stack<Integer>();
         stack1.push(1);
         stack1.push(2);
         stack1.push(3);
-        System.out.println("Stack 1 after push: "+stack1);
+        System.out.println("Stack 1 after push: " + stack1);
         stack1.pop();
-        System.out.println("Stack 1 after pop: "+stack1);
+        System.out.println("Stack 1 after pop: " + stack1);
         return stack1;
     }
 
-    public static Stack<String> stackStrings(){
+    public static Stack<String> stackStrings() {
         Stack<String> stack2 = new Stack<String>();
-        try{
+        try {
             stack2.pop();
-        }catch (EmptyStackException e){
+        } catch (EmptyStackException e) {
             System.out.println("Stack is empty");
         }
         stack2.push("Arjit");
         stack2.push("Karan");
         stack2.push("Sid");
         stack2.push("Laks");
-        System.out.println("Stack 2 after push: "+stack2);
+        System.out.println("Stack 2 after push: " + stack2);
         stack2.pop();
         stack2.pop();
-        System.out.println("Stack 2 after 2 pops: "+stack2);
+        System.out.println("Stack 2 after 2 pops: " + stack2);
         return stack2;
     }
-    public static void reverseIntegerStack(Stack<Integer> stack){
+
+    public static void reverseIntegerStack(Stack<Integer> stack) {
         Stack<Integer> revStack = new Stack<Integer>();
         int stackSize = stack.size();
-        for(int i=0;i<stackSize;i++){
+        for (int i = 0; i < stackSize; i++) {
             int pop = stack.peek();
             revStack.push(pop);
             stack.pop();
         }
-        System.out.println("Reverse Stack is: "+revStack);
+        System.out.println("Reverse Stack is: " + revStack);
     }
 
-    public static void reverseStringStack(Stack<String> stack){
+    public static void reverseStringStack(Stack<String> stack) {
         Stack<String> revStack = new Stack<String>();
         int stackSize = stack.size();
-        for(int i=0;i<stackSize;i++){
+        for (int i = 0; i < stackSize; i++) {
             String pop = stack.peek();
             revStack.push(pop);
             stack.pop();
         }
-        System.out.println("Reverse Stack is: "+revStack);
+        System.out.println("Reverse Stack is: " + revStack);
     }
 
-    public static void insertAtGivenIndexInGivenStack(int index, int value, Stack<Integer> stack){
+    public static void insertAtGivenIndexInGivenStack(int index, int value, Stack<Integer> stack) {
         Stack<Integer> stack1 = new Stack<Integer>();
-        int popTimes = stack.size()-index+1;
-        for(int i=1;i<=popTimes;i++){
+        int popTimes = stack.size() - index + 1;
+        for (int i = 1; i <= popTimes; i++) {
             stack1.push(stack.pop());
         }
         stack.push(value);
-        for(int i=1;i<=popTimes;i++){
+        for (int i = 1; i <= popTimes; i++) {
             stack.push(stack1.pop());
         }
-        System.out.println("Stack after insertion: "+stack);
+        System.out.println("Stack after insertion: " + stack);
     }
 
-    public static Integer minimumValueInStack(Stack<Integer> stack){
+    public static Integer minimumValueInStack(Stack<Integer> stack) {
         Stack<Integer> stack1 = new Stack<Integer>();
         int min = 0;
         int size = stack.size();
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             int current = stack.pop();
-            if(i==0){
+            if (i == 0) {
                 min = current;
             }
-            if(current<min){
+            if (current < min) {
                 min = current;
             }
         }
         return min;
     }
+
+    public static void sortStack(Stack<Integer> stack) {
+        System.out.println("Inside sort stack");
+        Stack<Integer> temp = new Stack<Integer>();
+        while (!stack.empty()) {
+            System.out.println("Inside sort stack");
+            int stack_current = stack.pop();
+            if (temp.empty()) {
+                temp.push(stack_current);
+            } else {
+                int temp_current;
+                while (!temp.isEmpty() && temp.peek() > stack_current) {
+                    temp_current = temp.pop();
+                    stack.push(temp_current);
+                }
+                temp.push(stack_current);
+            }
+        }
+        System.out.println("Sorted Stack: "+temp);
+    }
+
+//    public static Boolean checkBalancedString(String input){
+//        Stack<Character> stack = new Stack<Character>();
+//        for(int i=0;i<input.length();i++){
+//            char b = input.charAt(i);
+//            switch(b){
+//                case '[':
+//                    case '{':
+//                        case '(':
+//                            stack.push(b);
+//                            break;
+//                case ']':
+//                    char pop = stack.pop();
+//                    if(pop=='[' && b!=pop){
+//                        return false;
+//                    }
+//                    break;
+//                case '}':
+//                    char pop1 = stack.pop();
+//                    if(pop1=='{' && b!=pop1){
+//                        return false;
+//                    }
+//                    break;
+//                case ')':
+//                    char pop2 = stack.pop();
+//                    if(pop2=='(' && b!=pop2){
+//                        return false;
+//                    }
+//                    break;
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
 
     public static void main(String[] args) {
         stackIntegers();
@@ -98,5 +153,14 @@ public class StackPractice {
         insertAtGivenIndexInGivenStack(3,9,stack);
         insertAtGivenIndexInGivenStack(3,10,stack);
         System.out.println("Minimum value in this stack is: "+minimumValueInStack(stack));
+        Stack<Integer> stack2 = new Stack<Integer>();
+        stack2.push(11);
+        stack2.push(25);
+        stack2.push(55);
+        stack2.push(4);
+        sortStack(stack2);
+//        System.out.println("Check Balanced String: "+checkBalancedString("[2+{2+2}](4+5)"));
+//        System.out.println("Check Balanced String: "+checkBalancedString("[2+{2+2}(4+5)"));
+//        System.out.println("Check Balanced String: "+checkBalancedString("[2+2+2}]4+5)"));
     }
 }
